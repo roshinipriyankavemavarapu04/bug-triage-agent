d91751866bb1c30c3be188d11c4420a185cdbe85
Roshini Priyanka <Vemavarapu@msa.miraclesoft.com>
2026-07-03 23:27:45 +0530

    Implemented JWT authentication and protected APIs

---
+++ bug-triage-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-blue-600">
+          AI Bug Triage System
+        </h1>
+
+        <p className="mt-4 text-lg text-gray-600">
+          FastAPI + React + Gemini AI
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
